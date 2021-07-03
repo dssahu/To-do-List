d57db2c374b2288cf52fe06d56904d445299d6c1
dssahu <74927562+dssahu@users.noreply.github.com>
2021-07-04 02:12:18 +0530

    Initial Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <center>
+                <h3>This is about the page.</h3>
+                <p>
+                    Hi, I am Deepanwita Sahu, an aspiring Web Developer. This to-do list Application is made using ReactJS.
+                    <br />
+                    Before starting please make sure you have Node.js installed at your end.
+                    <br />
+                    You can run the project by typing 'npm start' on your terminal or command prompt (project directory)
+                    <br />
+                    You can also use 'npm run build' to build app for production.
+                    <br />
+                    Thank you
+                </p>
+            </center>
+        </div>
+    )
+}
